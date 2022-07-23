9f58e78c77ab058d13b112a1a10c790ccd45cb85
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-07-23 22:13:25 +0700

    styles

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -8,7 +8,7 @@
       <small>Текущий вес (кг)</small>
     </div>
 
-    <form @submit.prevent="addWeight">
+    <form class="weight__form" @submit.prevent="addWeight">
       <input type="number" step="0.1" v-model="weightInput">
       <input type="submit" value="Сохранить вес">
     </form>
@@ -112,6 +112,135 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+body {
+  background-color: #eee;
+}
+main {
+  padding: 1.5rem;
+}
+h1 {
+  font-size: 2em;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+h2 {
+  margin-bottom: 1rem;
+  color: #888;
+  font-weight: 400;
+}
+.weight {
+  &__current {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+    text-align: center;
+    background-color: white;
+    border-radius: 999px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+    border: 5px solid hwb(330 41% 0%);
+    margin: 0 auto 2rem;
 
+    span {
+      display: block;
+      font-size: 2em;
+      font-weight: bold;
+      margin-bottom: 0.5rem;
+    }
+
+    small {
+      color: #888;
+      font-style: italic;
+    }
+  }
+
+  &__form {
+    display: flex;
+    margin-bottom: 2rem;
+    border: 1px solid #AAA;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    transition: 200ms linear;
+    &:focus-within,
+    &:hover {
+      border-color: hotpink;
+      border-width: 2px;
+    }
+
+    input[type='number'] {
+      appearance: none;
+      outline: none;
+      border: none;
+      background-color: white;
+      flex: 1 1 0%;
+      padding: 1rem 1.5rem;
+      font-size: 1.25rem;
+    }
+
+    input[type="submit"] {
+      appearance: none;
+      outline: none;
+      border: none;
+      cursor: pointer;
+      background-color: hotpink;
+      padding: 0.5rem 1rem;
+      color: white;
+      font-size: 1.25rem;
+      font-weight: 700;
+      transition: 200ms linear;
+      border-left: 3px solid transparent;
+      &:hover {
+        background-color: white;
+        color: hotpink;
+        border-left-color: hotpink;
+      }
+    }
+  }
+  &__canvas {
+    width: 100%;
+    max-width: 720px;
+    background-color: white;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+    margin-bottom: 2rem;
+  }
+  &__history {
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.5rem;
+        cursor: pointer;
+        border: 1px solid hotpink;
+        border-bottom: none;
+        &:hover {
+          background-color: #f8f8f8;
+        }
+        &:last-of-type {
+          border: 1px solid hotpink;
+        }
+
+        span {
+          display: block;
+          font-size: 1.25rem;
+          font-weight: 700;
+          margin-right: 1rem;
+        }
+
+        small {
+          color: #888;
+          font-style: italic;
+        }
+      }
+    }
+  }
+}
 </style>
